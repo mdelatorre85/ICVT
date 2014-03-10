e6dc379e7115db2c9b39632441c9b769356a91cc
Cesar <cesar@gmail.com>
2014-03-09 18:12:18 -0600

    Se agregan las implementaciones de ExtractorConfiguration y ResultData para la fuente de datos de vocaciones

---
+++ core/src/main/java/mx/com/icvt/extraction/impl/vocaciones/VocacionesExtractorConfiguration.java
@@ -0,0 +1,13 @@
+package mx.com.icvt.extraction.impl.vocaciones;
+
+import mx.com.icvt.extraction.ExtractorConfiguration;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Cesar
+ * Date: 09/03/14
+ * Time: 18:04
+ */
+public class VocacionesExtractorConfiguration implements ExtractorConfiguration{
+
+}
---
+++ core/src/main/java/mx/com/icvt/extraction/impl/vocaciones/VocacionesResultData.java
@@ -0,0 +1,12 @@
+package mx.com.icvt.extraction.impl.vocaciones;
+
+import mx.com.icvt.extraction.ResultData;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Cesar
+ * Date: 09/03/14
+ * Time: 18:05
+ */
+public class VocacionesResultData implements ResultData {
+}
