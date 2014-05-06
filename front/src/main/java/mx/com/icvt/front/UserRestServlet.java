2073d084d41b83fdf3dbf08ed787fef6556acd9d
Cesar <cesar@gmail.com>
2014-05-06 00:07:55 -0500

    Se modifican algunas validaciones del método de logeo

--- front/src/main/java/mx/com/icvt/front/UserRestServlet.java
+++ front/src/main/java/mx/com/icvt/front/UserRestServlet.java
@@ -1,24 +1,22 @@
 package mx.com.icvt.front;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-import javax.persistence.PersistenceContext;
+import mx.com.icvt.persistence.impl.user.User;
+import mx.com.icvt.persistence.impl.user.UserResultData;
+import net.sf.json.JSONArray;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import mx.com.icvt.persistence.impl.user.UserResultData;
-import mx.com.icvt.persistence.impl.user.User;
-import net.sf.json.JSONArray;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-@PersistenceContext
 public class UserRestServlet extends HttpServlet {
 
     private class RestRequest {
-
         private Pattern regExAllPattern = Pattern.compile("/user/");
         private Pattern regExIdPattern = Pattern.compile("/user/([0-9a-z]*)");
 
@@ -76,16 +74,7 @@ public class UserRestServlet extends HttpServlet {
             throws ServletException, IOException {
 
         PrintWriter out = response.getWriter();
-        try {
-            RestRequest resourceValues = new RestRequest(request.getPathInfo());
-        } catch (ServletException e) {
-            response.setStatus(400);
-            response.resetBuffer();
-            e.printStackTrace();
-            out.println(e.toString());
-        }
-
-        UserResultData obj =new UserResultData();
+        UserResultData obj = new UserResultData();
         User us = new User();
 
         String user = request.getParameter("identity");
@@ -93,11 +82,11 @@ public class UserRestServlet extends HttpServlet {
 
         us.setIdentity(user);
         us.setPassword(pass);
-        obj.user=us;
+        obj.user = us;
 
         List<User> listUser = obj.login();
 
-        if(listUser!=null){
+        if (listUser != null) {
             JSONArray array = JSONArray.fromObject(listUser);
             out.println(array);
         }
