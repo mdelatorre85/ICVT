cffa47169ae3bc6667787d84a1e84adc687a8f26
Jair <dev.lnx1337@gmail.com>
2014-05-15 03:26:54 -0500

    news add etiquetas front

--- front/src/main/webapp/js/controllers.js
+++ front/src/main/webapp/js/controllers.js
@@ -12,6 +12,10 @@ sampleApp.controller('NewsController', function($scope,$http) {
         tracker : $scope.loadingTracker
       })
       .success(function(data, status, headers, config) {
+      		datea = data.noticias[0].fecha.split('-')
+      		$scope.newsaDay = datea[2]
+      		$scope.newsaMonth = datea[1]
+      		$scope.newsaYear = datea[0]
         	$scope.newsa = data.noticias[0].noticias;
         	$scope.newsb = data.noticias[1].noticias;
         	$scope.newsc = data.noticias[2].noticias;
