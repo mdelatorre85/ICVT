5af8da8950b49441d93f008795dc3ead21767bf1
Jair <dev.lnx1337@gmail.com>
2014-05-15 01:23:08 -0500

    jade news angular

---
+++ front/src/main/webapp/js/controllers.js
@@ -0,0 +1,19 @@
+sampleApp.controller('NewsController', function($scope,$http) {
+     
+	$scope.newsa = null;
+	$scope.newsb = null;	
+	$scope.newsc = null;
+
+	$http({
+        method  : 'POST',
+        url     : 'http://localhost:8080/api/v1/rest/noticias/agrupadas',
+        data    :  "",
+        headers : { 'Content-Type': 'application/x-www-form-urlencoded'},
+        tracker : $scope.loadingTracker
+      })
+      .success(function(data, status, headers, config) {
+        	$scope.newsa = data.noticias[0].noticias;
+        	$scope.newsb = data.noticias[1].noticias;
+        	$scope.newsc = data.noticias[2].noticias;
+      });
+});
