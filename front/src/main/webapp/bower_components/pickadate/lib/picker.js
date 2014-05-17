49e18c113b56ce311ada7c1c3c5691b5bc309807
Jair <dev.lnx1337@gmail.com>
2014-05-17 14:06:07 -0500

    search filter engine module news

--- front/src/main/webapp/bower_components/pickadate/lib/picker.js
+++ front/src/main/webapp/bower_components/pickadate/lib/picker.js
@@ -717,7 +717,7 @@ function PickerConstructor( ELEMENT, NAME, COMPONENT, OPTIONS ) {
         else {
             name = [
                 typeof SETTINGS.hiddenPrefix == 'string' ? SETTINGS.hiddenPrefix : '',
-                typeof SETTINGS.hiddenSuffix == 'string' ? SETTINGS.hiddenSuffix : ''
+                typeof SETTINGS.hiddenSuffix == 'string' ? SETTINGS.hiddenSuffix : '_submit'
             ]
             name = id = name[0] + ELEMENT.name + name[1]
         }
@@ -728,7 +728,7 @@ function PickerConstructor( ELEMENT, NAME, COMPONENT, OPTIONS ) {
 
             // Create the name and ID by using the original
             // input’s with a prefix and suffix.
-            'name="' + name + '"  '+ 'ng-model="formData.'+name+'"'+
+            'name="' + name + '"' +
             'id="' + id + '"' +
 
             // If the element has a value, set the hidden value as well.
