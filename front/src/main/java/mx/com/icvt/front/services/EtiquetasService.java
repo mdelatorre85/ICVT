c147c3f7c3c8a103f92c151f21ddf24d306c8a38
Cesar <cesar@gmail.com>
2014-05-19 21:38:01 -0500

    Se agrega servicio para consulta de Etiquetas

--- front/src/main/java/mx/com/icvt/front/services/EtiquetasService.java
+++ front/src/main/java/mx/com/icvt/front/services/EtiquetasService.java
@@ -5,12 +5,15 @@ import mx.com.icvt.persistence.impl.tags.EtiquetaRetriever;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/etiquetas")
 public class EtiquetasService {
     @GET
     @Path("/list")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
     public List<Etiqueta> getAll() {
         return new EtiquetaRetriever().getAllPersisted();
     }
