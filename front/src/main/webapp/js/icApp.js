922ab6009d603746e3082db55e577855bc6845c9
Gabo Lugo <glsmaster.xg@gmail.com>
2014-05-22 05:19:21 -0500

    Ajustes de estilos a login e IC, creamos meadia query para tablets

--- front/src/main/webapp/js/icApp.js
+++ front/src/main/webapp/js/icApp.js
@@ -32,12 +32,11 @@ function initMap() {
 	
 	var mapOptions = {
 		center: ueLatLng,
-		zoom: 12,
+		zoom: 11,
 		zoomControlOptions: {
 			style:google.maps.ZoomControlStyle.SMALL
 		},
 		styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"},{"saturation":-100},{"lightness":20}]},{"featureType":"road","elementType":"all","stylers":[{"visibility":"on"},{"saturation":-100},{"lightness":40}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"saturation":-10},{"lightness":30}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"visibility":"simplified"},{"saturation":-60},{"lightness":10}]},{"featureType":"landscape.natural","elementType":"all","stylers":[{"visibility":"simplified"},{"saturation":-60},{"lightness":60}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"},{"saturation":-100},{"lightness":60}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"},{"saturation":-100},{"lightness":60}]}],
-		panControl: false,
 		mapTypeControl: false,
 		streetViewControl: false,
 		rotateControl: false 
