a8a18fe18e4a646b2bb514398fd7185294cff422
Jair <dev.lnx1337@gmail.com>
2014-05-21 22:24:20 -0500

    test.sh

--- test.sh
+++ test.sh
@@ -2,3 +2,5 @@
  mvn clean install -f core/pom.xml -DskipTests
  mvn clean install -f impl/pom.xml -DskipTests
  mvn clean install -f front/pom.xml -DskipTests
+ asadmin undeploy icvt
+ asadmin deploy --name icvt --contextroot / front/target/front-1.0-SNAPSHOT.war
