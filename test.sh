4d360a45e0724d985341499c6006134aab2a6b37
Jair <dev.lnx1337@gmail.com>
2014-05-21 22:14:13 -0500

    test.sh

---
+++ test.sh
@@ -0,0 +1,4 @@
+ mvn clean install -f pom.xml -DskipTests
+ mvn clean install -f core/pom.xml -DskipTests
+ mvn clean install -f impl/pom.xml -DskipTests
+ mvn clean install -f front/pom.xml -DskipTests
